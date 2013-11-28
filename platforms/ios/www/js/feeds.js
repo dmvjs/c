3b08d3717c36db934718aac9a05e43222a34fe3a
Kirk Elliott <kirk@dmvjs.com>
2013-11-27 20:57:32 -0500

    correct file writing & reading implementation

--- platforms/ios/www/js/feeds.js
+++ platforms/ios/www/js/feeds.js
@@ -10,51 +10,63 @@ $(window).on('go', function (e) {
 
   window.app.feedServices = {
     fs: null,
-    feedJSON: {},
-    imageCounter: 0,
-    imageCounterMax: 0,
+    activeFile: null,
+    activeWriter: null,
+    activeFeed: 0,
+    existingFile: null,
+    workingFeed: 0,
     imageURLList: [],
-    onFileSystemSuccess: function (fileSystem) {
-      window.app.feedServices.fs = fileSystem;
 
-      //window.app.feedServices.getFeed(window.app.feeds[0]);
-      $.getJSON(window.app.feeds[0].url,
-        function(data){
-          window.app.feedServices.feedJSON = data;
-          window.app.feedServices.createFile('feed.json')
-        })
-        //.success(function() { alert("second success"); })
-        //.error(function() { alert("error"); })
-        //.complete(function() { alert("complete"); });
-    },
-    onFileSystemFail: function (event) {
-      alert(event.target.error.code);
-    },
-    getFile: function (url) {
-      var fileTransfer = new FileTransfer(),
-        uri = encodeURI(url);
 
-      fileTransfer.download(
-        uri,
-        window.app.feedServices.fs.root.fullPath + '/' + url.split('/').pop(),
-        function(entry) {
-          alert("download complete: " + entry.fullPath);
-          window.app.feedServices.imageCounter += 1;
-          if (window.app.feedServices.imageCounter < window.app.feedServices.imageURLList.length) {
-            window.app.feedServices.getFile(window.app.feedServices.imageURLList[window.app.feedServices.imageCounter]);
+    init: function (feedId) {
+      var feedId = feedId || 0,
+        filename = 'feed' + window.app.feeds[feedId].id + '.json';
+
+      if (window.app.connect.online) {
+        $.getJSON(
+          window.app.feeds[feedId].url,
+          window.app.feedServices.writeLocalFeed
+        )
+      }
+
+      window.app.feedServices.loadFile(
+        filename,
+        window.app.feedServices.gotReadFileEntry,
+        function () {
+          if (!window.app.connect.online) {
+            alert('no connection & no local file');
           } else {
-            window.app.feedServices.imageCounter = 0;
-            alert('all done');
+            alert('making file');
+            window.app.feedServices.createFile(
+              filename
+            );
           }
-        },
-        function(error) {
-          alert("download error source " + error.source);
-          /*alert("download error target " + error.target);
-          alert("download error code" + error.code);*/
-        },
-        false
+        });
+    },
+
+    loadFile: function (filename, callback, fail) {
+      window.app.feedServices.fs.root.getFile(
+        filename,
+        null,
+        callback || window.app.feedServices.gotFileEntry,
+        fail || window.app.feedServices.fileError
       );
     },
+
+    readFile: function (file) {
+      var reader = new FileReader();
+      reader.onloadend = function (e) {
+        var text = JSON.stringify(e.target.result);
+        alert(text.substr(0, 40))
+      };
+      reader.readAsText(file);
+    },
+
+    writeLocalFeed: function (data) {
+      var text = JSON.stringify(data);
+      window.app.feedServices.activeWriter.write(JSON.stringify(data));
+    },
+
     createFile: function (filename) {
       window.app.feedServices.fs.root.getFile(
         filename,
@@ -62,34 +74,97 @@ $(window).on('go', function (e) {
           create: true,
           exclusive: false
         },
-        window.app.feedServices.gotFileEntry,
+        window.app.feedServices.gotFile,
         window.app.feedServices.fileError
       );
     },
+
+    gotReadFileEntry: function (fileEntry) {
+      fileEntry.file(
+        window.app.feedServices.gotReadFile,
+        window.app.feedServices.fileError
+      );
+    },
+
     gotFileEntry: function (fileEntry) {
-      fileEntry.createWriter(
+      fileEntry.file(
+        window.app.feedServices.gotFile,
+        window.app.feedServices.fileError
+      );
+    },
+
+    gotReadFile: function (file) {
+      window.app.feedServices.existingFile = file;
+      window.app.feedServices.readFile(file);
+    },
+
+    gotFile: function (file) {
+      window.app.feedServices.activeFile = file;
+      file.createWriter(
         window.app.feedServices.gotFileWriter,
         window.app.feedServices.fileError
       );
     },
-    gotFileWriter: function (writer) {
-      writer.onwriteend = function(evt) {
-        window.app.feedServices.downloadImages();
-      };
-      writer.write(JSON.stringify(window.app.feedServices.feedJSON));
-    },
-    fileError: function (error) {
-      console.log(error.code);
-    },
-    downloadImages: function () {
-      var feed = window.app.feedServices.feedJSON.rss.channel.item;
-      //window.app.feedServices.imageCounterMax = feed.length;
-      for (var i = 0; i < feed.length; i += 1) {
-        window.app.feedServices.imageURLList.push(feed[i].image);
-      }
-      window.app.feedServices.getFile(window.app.feedServices.imageURLList[0]);
-    }
 
+    gotFileWriter: function (writer) {
+      window.app.feedServices.activeWriter = writer;
+      writer.onwriteend = function(e) {
+        alert('writer done');
+        window.app.feedServices.gotReadFileEntry(window.app.feedServices.activeFile);
+        //window.app.feedServices.readFile(window.app.feedServices.activeFile);
+        //window.app.feedServices.downloadImages();
+      };
+    },
+
+
+
+
+
+
+    createImageURLList: function () {
+      var feed = window.app.feedServices.feedJSON.rss.channel.item;
+
+      $.each(feed, function (index, element) {
+        window.app.feedServices.imageURLList.push(element.image);
+      })
+
+      alert('imageURLList created')
+    },
+
+    downloadImages: function () {
+      var file = window.app.feedServices.imageURLList.shift();
+      if (window.app.feedServices.imageURLList.length) {
+        window.app.feedServices.downloadFile(file);
+      } else {
+        alert('all done');
+      }
+    },
+
+    downloadFile: function (url, callback) {
+      var fileTransfer = new FileTransfer(),
+        uri = encodeURI(url);
+
+      fileTransfer.download(
+        uri,
+        window.app.feedServices.fs.root.fullPath + '/' + url.split('/').pop(),
+        callback,
+        window.app.feedServices.fileError,
+        false
+      );
+    },
+
+    fileError: function (error) {
+      alert('error')
+    },
+
+    onFileSystemSuccess: function (fs) {
+      window.app.feedServices.fs = fs;
+      window.app.feedServices.init();
+    },
+
+    onFileSystemFail: function (event) {
+      alert(event.target.error.code);
+    }
   }
 
   window.requestFileSystem(
@@ -100,14 +175,3 @@ $(window).on('go', function (e) {
   );
 
 });
-
-function gotFileWriter(writer) {
-  writer.onwriteend = function(evt) {
-    alert('file complete');
-  };
-  writer.write("some sample text");
-}
-
-function fail(error) {
-  console.log(error.code);
-}
